045c89c55a57a78a0ecb9a9876f9a16a2478c1ef
arman29kz <arman29kz@users.noreply.github.com>
2020-07-18 01:21:03 +0500

    testing footer

--- bot.js
+++ bot.js
@@ -147,8 +147,14 @@ async function sendMessage () {
       fieldText += `${mate.flag} [${mate.name}](${mate.swgoh})\n`; // Discord automatically trims messages
     }
     embed.addField(fieldName, fieldText, true);
-    {name: 'Inline field title', value: 'Some value here', inline: true };
-  }  
+    embed.addField('Inline field title', 'Some value here', true);
+  }
+  embed.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)  
   embed.setDescription(desc);  
   embed.setFooter('Last refresh', 'https://swgoh.gg/game-asset/u/CHOPPERS3/');
   embed.setTimestamp();
