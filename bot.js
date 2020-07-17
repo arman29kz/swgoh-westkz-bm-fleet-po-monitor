24e7e330988370f6357c5520df36ea9298d2518c
arman29kz <arman29kz@users.noreply.github.com>
2020-07-18 01:28:42 +0500

    Title updated

--- bot.js
+++ bot.js
@@ -148,12 +148,6 @@ async function sendMessage () {
     }
     embed.addField(fieldName, fieldText, true);    
   }
-  embed.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	);
   embed.setDescription(desc);  
   embed.setFooter('Last refresh', 'https://swgoh.gg/game-asset/u/CHOPPERS3/');
   embed.setTimestamp();
