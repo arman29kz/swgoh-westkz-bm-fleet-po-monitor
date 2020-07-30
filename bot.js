1820d3d4e7b3d38ec7ffda8282240ae5e018c6b2
arman29kz <arman29kz@users.noreply.github.com>
2020-07-31 02:03:01 +0500

    setAuthor has been added

--- bot.js
+++ bot.js
@@ -137,18 +137,18 @@ function calculateSecondsUntilPayout () {
 }
 
 async function sendMessage () {
-  let embed = new Discord.RichEmbed();  
+  let embed = new Discord.RichEmbed();
+  embed.setAuthor('You can support me on Patreon', 'https://swgoh.gg/game-asset/u/CHOPPERS3/', 'https://www.patreon.com/bmbots')
   embed.setTitle('Fleet PO Monitor. Updates every 1 min. Next in:');
-  let desc = 'Changed PO time? Pls contact to <@443487478091874324>.  Thnx';
-  for (let i in mates) {    
+  embed.setDescription('Changed PO time? Pls contact to <@443487478091874324>.  Thnx');
+  for (let i in mates) {
     let fieldName = String(mates[i].time) + " - (UTC " + String(mates[i].po.hours).padStart(2, '00') + ":" + String(mates[i].po.minutes).padStart(2, '00') + ")";
     let fieldText = '';
     for (const mate of mates[i].mates) {
       fieldText += `${mate.flag} [${mate.name}](${mate.swgoh})\n`; // Discord automatically trims messages
     }
     embed.addField(fieldName, fieldText, true);    
-  }  
-  embed.setDescription(desc);  
+  }
   embed.setFooter('Last refresh', 'https://swgoh.gg/game-asset/u/CHOPPERS3/');
   embed.setTimestamp();
   await message.edit({embed});
