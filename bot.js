a531cbc51f959fd5808b77ac88e2f9da38f73e43
arman29kz <arman29kz@users.noreply.github.com>
2020-08-01 00:52:43 +0500

    SetDescription has been corrected, extra line added

--- bot.js
+++ bot.js
@@ -139,8 +139,7 @@ function calculateSecondsUntilPayout () {
 async function sendMessage () {
   let embed = new Discord.RichEmbed();
   embed.setTitle('Fleet Payout Monitor. Updates every 1 minute. Next in:');
-  embed.setDescription('Changed PO time? Pls contact to <@443487478091874324>. Thnx' +
-      '[Patreon support and donation](https://www.patreon.com/bmbots) | [Bot status](https://www.nixstats.com/report/5f21c98c997820301d4213bc?m=5e92ea64b17639391d37ab93)');
+  embed.setDescription('Changed PO time? Pls contact to <@443487478091874324>. Thnx' + '\n' + '[Patreon support and donation](https://www.patreon.com/bmbots) | [Bot status](https://www.nixstats.com/report/5f21c98c997820301d4213bc?m=5e92ea64b17639391d37ab93)');
   for (let i in mates) {
     let fieldName = String(mates[i].time) + " - (UTC " + String(mates[i].po.hours).padStart(2, '00') + ":" + String(mates[i].po.minutes).padStart(2, '00') + ")";
     let fieldText = '';
